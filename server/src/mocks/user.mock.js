6cbf47e4a3cd36600beb3e478517e0c43f5239ed
francisco estenio <estenio_gino@yahoo.com.br>
2026-07-12 11:33:52 -0300

    feat: implementar dados simulados

---
+++ server/src/mocks/user.mock.js
@@ -0,0 +1,19 @@
+const users = [{
+    id: 1,
+    name: 'silva',
+    email: 'silva@gmail.com',
+    status: 'true',
+}, {
+    id: 2,
+    name: 'maria',
+    email: 'maria@gmail.com',
+    status: true
+}, {
+    id: 3,
+    name: 'gino',
+    email: 'gino@gmail.com',
+    status: true
+
+}];
+
+export default users;
