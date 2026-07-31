7a7d72914800638fb19dec977b5229b0e4b91d8b
francisco estenio <estenio_gino@yahoo.com.br>
2026-07-30 21:56:39 -0300

    feat :'implementar interface grafica para busca de usuarios'

--- client/app.js
+++ client/app.js
@@ -1,3 +1,23 @@
+const imputId = document.queryselector('button');
+const botaoBuscar = document.queryselector('input');
+const resultado = document.querySelector('p');
+
+button.addEventListener('click', async () => {
+
+    const inputVlaue = Number(input.value);
+
+    const data = await getuser();
+
+    const user = data.find(user => user.id === inputValue);
+
+    if (!user) {
+
+        return paragraph.textContent = `Usuário de id ${inputValue} não encontrado`;
+    }
+    paragraph.textContent = `Usuário ${user.name} encontrado`;
+    
+    });
+    
 async function getUsers() {
 try {
     const response = await fetch('http://localhost:3000/users');
@@ -22,3 +42,4 @@ try {
 }
 
 getUsers();
+
